48e98a0cdf0c532d99ba77440942d879b6aae3e2
ibnjunaid <osamabinjunaid36@gmail.com>
2024-02-11 16:34:13 +0000

    Initial commit -added instrumentation

---
+++ run.sh
@@ -0,0 +1,1 @@
+java -javaagent:disco/disco-java-agent.jar=pluginPath=disco/disco-plugins:loggerfactory=software.amazon.disco.agent.reflect.logging.StandardOutputLoggerFactory:verbose -jar build/libs/demo-0.0.1-SNAPSHOT.jar
